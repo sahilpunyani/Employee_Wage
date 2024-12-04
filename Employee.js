1ee9f9d02ea7d9a87f5665c51d87ca75e901c861
Sahil Punyani <sahilpunyani0909@gmail.com>
2024-12-04 14:27:43 +0530

    task2

--- Employee.js
+++ Employee.js
@@ -1,7 +1,20 @@
-let res = Math.floor(Math.random()*2)
-if(res == 0){
-    console.log("absent");
-}else{
-    console.log("present");
+let res = Math.floor(Math.random() * 2);
+if (res == 0) {
+    console.log("Absent");
+} else {
+    console.log("Present");
+    
+    let timeWork = Math.floor(Math.random() * 3);
+    switch(timeWork) {
+        case 0:
+            console.log("No Time = 0 $");
+            break;
+        case 1:
+            console.log("Part Time = 80 $");
+            break;
+        case 2:
+            console.log("Full Time = 160 $");
+            break;
+    }
 }
-// console.log(res);
+
